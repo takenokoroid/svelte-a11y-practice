a635d096d2f186be837f7b3d343eba21c70c80d3
Takaeroid <takenokoroid@gmail.com>
2022-06-05 03:12:42 +0900

    todo追加モーダルの作成

---
+++ src/Organisms/AddTodoModal.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Form from "../Atoms/Form.svelte";
+  import Box from "../Atoms/Box.svelte";
+  import Heading from "../Atoms/Heading.svelte";
+  import Modal from "../Molecules/Modal.svelte";
+  import Text from "../Atoms/Text.svelte";
+  import Button from "../Atoms/Button.svelte";
+  import { useTodo } from "../store/useTodo";
+
+  const todo = useTodo;
+  let title = "";
+  let detail = "";
+  let deadline = "";
+
+  const addTodoItem = () => {
+    const dict = {
+      title: title,
+      detail: detail,
+      deadline: deadline,
+    };
+    todo.addTodo(dict);
+  };
+</script>
+
+<Modal>
+  <Heading element="h2">Todoを追加する</Heading>
+  <Box>
+    <Text>タイトル</Text>
+    <Form bind:value={title} />
+  </Box>
+  <Box>
+    <Text>詳細</Text>
+    <Form bind:value={detail} />
+  </Box>
+  <Box>
+    <Text>期限</Text>
+    <Form bind:value={deadline} />
+  </Box>
+  <Button onClick={addTodoItem}>追加する</Button>
+  <Button>キャンセル</Button>
+</Modal>
