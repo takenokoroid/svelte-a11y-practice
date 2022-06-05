058571df9bf5f0801133cf7958983694ffe9342e
Takaeroid <takenokoroid@gmail.com>
2022-06-05 15:03:35 +0900

    CSSの適応

--- src/Organisms/TodoItem.svelte
+++ src/Organisms/TodoItem.svelte
@@ -16,11 +16,60 @@
   };
 </script>
 
-<Box>
+<Box class="todoitem-item">
   <Form type="checkbox" />
-  <Text>{title}</Text>
-  <Text>期限:{deadline}</Text>
-  <Text>{detail}</Text>
-  <Button onClick={handleOpenEditTodoModal}>編集</Button>
-  <Button onClick={handleOpenDeleteTodoModal}>削除</Button>
+  <Box class="todoitem-todo">
+    <Box class="todoitem-title">
+      <Text class="todoitem-title-text">{title}</Text>
+    </Box>
+    <Box class="todoitem-detail">
+      <Text class="todoitem-title-detail">{detail}</Text>
+    </Box>
+    <Box class="todoitem-deadline">
+      <Text class="todoitem-title-deadline">期限:{deadline}</Text>
+    </Box>
+    <Box class="todoitem-buttons">
+      <Button onClick={handleOpenEditTodoModal}>編集</Button>
+      <Button onClick={handleOpenDeleteTodoModal}>削除</Button>
+    </Box>
+  </Box>
 </Box>
+
+<style>
+  :global(.todoitem-item) {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+  }
+  :global(.todoitem-todo) {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  :global(.todoitem-title) {
+    width: 50%;
+  }
+  :global(.todoitem-detail) {
+    width: 50%;
+  }
+  :global(.todoitem-deadline) {
+    width: 50%;
+  }
+  :global(.todoitem-buttons) {
+    width: 50%;
+  }
+  :global(.todoitem-title-text) {
+    font-weight: bold;
+    font-size: 18pt;
+    margin: 0px;
+  }
+  :global(.todoitem-title-detail) {
+    font-size: 12pt;
+    margin: 0px;
+  }
+  :global(.todoitem-title-deadline) {
+    font-size: 12pt;
+    margin: 0px;
+  }
+</style>
